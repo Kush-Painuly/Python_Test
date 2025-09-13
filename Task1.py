ffb88850c3c328948182ed1a5069e4189da4fa97
Kush Painuly <160495043+Kush-Painuly@users.noreply.github.com>
2025-09-13 17:23:27 +0530

    first commit

---
+++ Task1.py
@@ -0,0 +1,8 @@
+#Accepting value from user
+num1 = int(input("Enter the value of the first variable: "))
+num2 = int(input("Enter the value of the second variable: "))
+
+print("Addition of both the variable:", num1+num2)
+print("Subtraction of both the variable:", num1-num2)
+print("Multiplication of both the variable:", num1*num2)
+print("Division of both the variable:", num1/num2)
---
+++ Task2.py
@@ -0,0 +1,7 @@
+
+f_name = input("Enter your First Name: ")
+l_name = input("Enter your Last Name: ")
+
+full_name = f_name +" "+ l_name
+
+print(f"Hello, {full_name}! Welcome to the Python Program.")

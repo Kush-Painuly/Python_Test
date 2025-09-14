09fcd506ddbdaae1fa285cd47118c08cf3461d55
Kush Painuly <160495043+Kush-Painuly@users.noreply.github.com>
2025-09-14 13:57:24 +0530

    new assignments

---
+++ Control Structures/Loops/Task1.py
@@ -0,0 +1,7 @@
+user_inp = int(input("Enter your number to check whether it is even or odd: "))
+
+if user_inp % 2 ==0:
+    print(f"{user_inp} is an Even Number")
+else:
+    print(f"{user_inp} is an Odd Number")
+    
